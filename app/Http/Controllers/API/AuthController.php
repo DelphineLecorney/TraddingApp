6c1404ee5942accbec845c557d7806a452db0003
DelphineLecorney <lecorney.delphine@gmail.com>
2023-08-07 12:15:08 +0200

    Add function login in AuthController

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -7,5 +7,35 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+        $credentials = $request->only('email', 'password');
+        $token = Auth::attempt($credentials);
+
+        if(!token){
+            return response()->json([
+                'message' => 'Unauthorized',
+            ], 401);
+        }
+
+        $user = Auth::user();
+        return response()->json([
+            'user' => $user,
+            'authorization' => [
+                'token' => $token,
+                'type' => 'bearer',
+            ]
+            ]);
+    }
+
+    
 }
